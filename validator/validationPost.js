2f857298d96d6ad6238a5a66c6afcb722cf6be7d
Parkyuchan <dbcks4848@naver.com>
2025-03-06 03:10:48 +0900

    게시글 리스트 완료

---
+++ validator/validationPost.js
@@ -0,0 +1,11 @@
+// 입력값이 채워졌는지 확인하는 함수
+export function validateTitle(input) {
+
+    if (!input.value.trim()) {
+        input.classList.add("invalid");
+        return false;
+    } else {
+        input.classList.remove("invalid");
+        return true;
+    }
+}
